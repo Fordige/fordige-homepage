e749fb80cf0cff4d2b07369a7985566ef37f6eac
da1ni2 <hywang1141005@gmail.com>
2025-04-11 16:35:48 +0800

    modify language

--- frontend/src/locales/zh.js
+++ frontend/src/locales/zh.js
@@ -1,7 +1,7 @@
 export const zh = {
   head_about_us: "關於我們",
   head_service_content: "服務內容",
-  head_cooperate_flow: "專案流程",
+  head_cooperate_process: "專案流程",
   head_contact_us: "聯絡我們",
 
   homepage_content: "從形象網頁到智能應用，協助您的業務只需 25,000 起",
