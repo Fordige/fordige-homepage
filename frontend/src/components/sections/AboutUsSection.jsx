7f3101578cfdc4d98fe9b0dfaf9891aa2df41db7
da1ni2 <hywang1141005@gmail.com>
2025-04-12 16:27:09 +0800

    update trasition animation

--- frontend/src/components/sections/AboutUsSection.jsx
+++ frontend/src/components/sections/AboutUsSection.jsx
@@ -1,9 +1,7 @@
 import { forwardRef, useState } from "react";
 
-import ContactUsButton from "../ui/ContactUsButton";
 import rightButton from "../../assets/aboutUs/right-button.svg";
 import leftButton from "../../assets/aboutUs/left-button.svg";
-
 import bg from "../../assets/aboutUs/bg.webp";
 import bg2 from "../../assets/aboutUs/bg2.webp";
 
@@ -16,64 +14,62 @@ const AboutUsSection = forwardRef((props, ref) => {
   const handlePrev = () => {
     setCurrentPage((prevIndex) => prevIndex - 1);
   };
+
   return (
-    <>
+    <section
+      className="relative mb-[5rem] mt-[5rem] flex min-h-[calc(100vh-10rem)] w-full flex-col items-center justify-center overflow-hidden bg-highlight dark:bg-shadow3"
+      ref={ref}
+    >
+      <img
+        className={`absolute h-full w-full object-fill transition-opacity duration-1000 ease-in-out ${
+          currentPage === 0 ? "opacity-100" : "opacity-0"
+        }`}
+        src={bg}
+        alt="bg"
+      />
+      <img
+        className={`absolute h-full w-full object-fill transition-opacity duration-1000 ease-in-out ${
+          currentPage === 1 ? "opacity-100" : "opacity-0"
+        }`}
+        src={bg2}
+        alt="bg2"
+      />
       {currentPage === 0 ? (
-        <section
-          className={`relative mb-[5rem] mt-[5rem] flex min-h-[calc(100vh-10rem)] w-full flex-col items-center justify-center overflow-hidden bg-highlight dark:bg-shadow3`}
-          ref={ref}
+        <button
+          className="absolute right-[1rem] top-1/2 z-10 -translate-y-1/2 opacity-25 hover:opacity-100"
+          onClick={handleNext}
         >
           <img
-            className="absolute h-full w-full object-fill"
-            src={bg}
-            alt="bg"
+            className="h-[5rem] w-[5rem]"
+            src={rightButton}
+            alt="rightButton"
           />
-          <button
-            className="absolute right-[1rem] top-1/2 -translate-y-1/2 opacity-25 hover:opacity-100"
-            onClick={handleNext}
-          >
-            <img
-              className="h-[5rem] w-[5rem]"
-              src={rightButton}
-              alt="rightButton"
-            />
-          </button>
-          <div className="absolute bottom-[1rem] left-1/2 flex h-[1.5rem] w-[3rem] -translate-x-1/2 items-center justify-evenly rounded-[1rem] bg-white">
-            <div className="h-[0.5rem] w-[0.5rem] rounded-full bg-shadow3"></div>
-            <div className="h-[0.5rem] w-[0.5rem] rounded-full bg-shadow2"></div>
-          </div>
-        </section>
+        </button>
       ) : (
-        <section
-          className={`relative mb-[5rem] mt-[5rem] flex min-h-[calc(100vh-10rem)] w-full flex-col items-center justify-center overflow-hidden bg-highlight dark:bg-shadow3`}
-          ref={ref}
+        <button
+          className="absolute left-[1rem] top-1/2 z-10 -translate-y-1/2 opacity-25 hover:opacity-100"
+          onClick={handlePrev}
         >
           <img
-            className="absolute h-full w-full object-fill"
-            src={bg2}
-            alt="bg2"
+            className="h-[5rem] w-[5rem]"
+            src={leftButton}
+            alt="leftButton"
           />
-          <button
-            className="absolute left-[1rem] top-1/2 -translate-y-1/2 opacity-25 hover:opacity-100"
-            onClick={handlePrev}
-          >
-            <img
-              className="h-[5rem] w-[5rem]"
-              src={leftButton}
-              alt="leftButton"
-            />
-          </button>
-          <div className="absolute bottom-[1rem] left-1/2 flex h-[1.5rem] w-[3rem] -translate-x-1/2 items-center justify-evenly rounded-[1rem] bg-white">
-            <div
-              className={`h-[0.5rem] w-[0.5rem] rounded-full ${currentPage === 0 ? "bg-shadow3" : "bg-shadow2"}`}
-            ></div>
-            <div
-              className={`h-[0.5rem] w-[0.5rem] rounded-full ${currentPage === 1 ? "bg-shadow3" : "bg-shadow2"}`}
-            ></div>
-          </div>
-        </section>
+        </button>
       )}
-    </>
+      <div className="absolute bottom-[1rem] left-1/2 z-10 flex h-[1.5rem] w-[3rem] -translate-x-1/2 items-center justify-evenly rounded-[1rem] bg-white">
+        <div
+          className={`h-[0.5rem] w-[0.5rem] rounded-full ${
+            currentPage === 0 ? "bg-shadow3" : "bg-shadow2"
+          }`}
+        ></div>
+        <div
+          className={`h-[0.5rem] w-[0.5rem] rounded-full ${
+            currentPage === 1 ? "bg-shadow3" : "bg-shadow2"
+          }`}
+        ></div>
+      </div>
+    </section>
   );
 });
 
