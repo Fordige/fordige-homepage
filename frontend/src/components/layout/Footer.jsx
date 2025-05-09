b606114d2e39920a68cd70afc22ccad418058b60
da1ni2-sci <yoyopkr1141005@gmail.com>
2025-05-09 14:34:27 +0800

    update animation button

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -5,7 +5,6 @@ import bannerDark from "../../assets/footer/banner-dark.svg";
 import bannerWhite from "../../assets/footer/banner-white.svg";
 
 import useModeStore from "../../store/modeStore";
-import chicken from "../../assets/aboutUs/chicken.gif";
 
 function Footer() {
   const openModal = useModalStore((state) => state.openModal);
@@ -32,7 +31,6 @@ function Footer() {
         Copyright © 2025 Fordige. All rights reserved.
       </div>
       <div className="flex">
-        <img className="h-[4.56vh] md:hidden" src={chicken} alt="chicken" />
         <img
           className="h-[4.56vh] w-[10vw] cursor-pointer md:h-[4rem] md:w-[4rem]"
           src={lineIcon}
--- frontend/src/components/sections/AboutUsSection.jsx
+++ frontend/src/components/sections/AboutUsSection.jsx
@@ -1,17 +1,23 @@
 import { forwardRef, useRef } from "react";
+import starter4444 from "../../assets/aboutUs/starter.mov";
 import starter from "../../assets/aboutUs/starter.webm";
-
-// text animation
+import titleAnimation4444 from "../../assets/aboutUs/title-animation.mov";
 import titleAnimation from "../../assets/aboutUs/title-animation.webm";
 
 const AboutUsSection = forwardRef((props, ref) => {
   const videoRef = useRef(null);
 
   return (
-    <div ref={ref}>
+    <div className="h-full w-full" ref={ref}>
       {/* 手機 */}
       <section className="flex h-[60vh] w-[130vw] flex-col items-center justify-around bg-highlight dark:bg-shadow3 md:hidden">
-        <video className="w-[60vw] self-start" muted autoPlay playsInline>
+        <video
+          className="ml-[15vw] w-[60vw] self-start"
+          muted
+          autoPlay
+          playsInline
+        >
+          <source src={titleAnimation4444} />
           <source src={titleAnimation} type="video/webm" />
         </video>
 
@@ -22,16 +28,19 @@ const AboutUsSection = forwardRef((props, ref) => {
           playsInline
           autoPlay
         >
+          <source src={starter4444} />
           <source src={starter} type="video/webm" />
         </video>
       </section>
       {/* 桌面 */}
       <section className="relative hidden h-[90vh] w-full flex-col items-center justify-center gap-[1rem] bg-highlight dark:bg-shadow3 md:flex">
         <video className="w-[35rem] self-start" muted autoPlay playsInline>
+          <source src={titleAnimation4444} />
           <source src={titleAnimation} type="video/webm" />
         </video>
         <div className="h-full w-full">
           <video ref={videoRef} muted playsInline autoPlay>
+            <source src={starter4444} />
             <source src={starter} type="video/webm" />
           </video>
         </div>
