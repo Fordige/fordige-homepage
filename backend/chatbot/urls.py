fbdbba436b2f673a682e81241eae1dbcafc9496c
da1ni2-sci <yoyopkr1141005@gmail.com>
2025-05-13 16:40:53 +0800

    line test

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import line_webhook
 
 urlpatterns = [
-    path('line/webhook/', line_webhook, name='line_webhook'),
+    path('webhook/', line_webhook, name='line_webhook'),
 ]
