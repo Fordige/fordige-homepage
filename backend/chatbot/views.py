53e9c37d31245862e31c51a4f8a64dc5b7686c31
da1ni2 <hywang1141005@gmail.com>
2025-05-13 18:37:15 +0800

    update line test

--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -8,21 +8,21 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(settings.LINE_CHANNEL_SECRET)
 
-@csrf_exempt
-def line_webhook(request):
-    if request.method == 'POST':
-        signature = request.META.get('HTTP_X_LINE_SIGNATURE')
-        body = request.body.decode('utf-8')
-
-        try:
-            handler.handle(body, signature)
-        except InvalidSignatureError:
-            return HttpResponse(status=400)
-
-        return HttpResponse(status=200)
-    return HttpResponse(status=400)
+# @csrf_exempt
 # def line_webhook(request):
-#     return HttpResponse("Hello, this is the LINE webhook endpoint.")
+#     if request.method == 'POST':
+#         signature = request.META.get('HTTP_X_LINE_SIGNATURE')
+#         body = request.body.decode('utf-8')
+
+#         try:
+#             handler.handle(body, signature)
+#         except InvalidSignatureError:
+#             return HttpResponse(status=400)
+
+#         return HttpResponse(status=200)
+#     return HttpResponse(status=400)
+def line_webhook(request):
+    return HttpResponse("Hello, this is the LINE webhook endpoint.")
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
