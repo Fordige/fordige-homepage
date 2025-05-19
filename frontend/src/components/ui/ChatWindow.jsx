3a81938d8340bae2c530a2299bd82d2a4762d916
da1ni2 <hywang1141005@gmail.com>
2025-05-19 15:44:20 +0800

    update changes

--- frontend/src/components/ui/ChatWindow.jsx
+++ frontend/src/components/ui/ChatWindow.jsx
@@ -57,10 +57,10 @@ const ChatWindow = ({ onClose }) => {
 
     isConnecting.current = true;
     console.log(
-      `嘗試連線 WebSocket: wss://fordige.com/ws/chatbot/${sessionId}/`,
+      `嘗試連線 WebSocket: wss://ws.fordige.com/ws/chatbot/${sessionId}/`,
     );
     const newSocket = new WebSocket(
-      `wss://fordige.com/ws/chatbot/${sessionId}/`,
+      `wss://ws.fordige.com/ws/chatbot/${sessionId}/`,
     );
     setSocket(newSocket);
 
