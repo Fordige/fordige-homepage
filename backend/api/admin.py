d0ee01b9e2db2b1ece1cb5a72de2cf402707104c
da1ni2 <hywang1141005@gmail.com>
2025-05-21 01:51:24 +0800

    clean space

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -4,5 +4,5 @@ from api.models import CaseModel
 class CaseModelAdmin(admin.ModelAdmin):
     list_display = ['case_id', 'case_title', 'created_dt', 'finished_dt', 'name', 'line_id', 'email', 'budget', 'service', 'price' ]
 
-# Register your models here.
+# Register your models here.,
 admin.site.register(CaseModel, CaseModelAdmin)
